21c9cae5b9737e86c381ab8691a23441e3b4ac5e
sachin saini <sachinsaini18585@gmail.com>
2026-07-09 10:00:50 +0530

    Update URL

--- public/chatBot.js
+++ public/chatBot.js
@@ -1,6 +1,6 @@
 (function (){
 
-    const api_Url="http://localhost:3000/api/chat"
+    const api_Url="https://support-ai-ashy-eight.vercel.app/api/chat"
     const scriptTag=document.currentScript;
     const ownerId=scriptTag.getAttribute("data-owner-id")
     if(!ownerId){
